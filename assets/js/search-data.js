f74a4758ce119cb2794e2ea1ca8a654eaab2a8e1
yhc-1 <59877970+yhc-1@users.noreply.github.com>
2025-05-19 16:54:30 +0000

    Deploying to gh-pages from @ yhc-1/test-web@d958e7015692cbb5e4af91df31bfe9aeceb47dd7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -105,30 +105,4 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/test-web/projects/9_project/";
-            },},{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+            },},];
